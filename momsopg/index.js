1f3174f9fe3236a74d647fb017d1b026cb727dfe
Marie Wulff <mawu0001@stud.kea.dk>
2024-09-20 08:46:37 +0200

    ændring

--- momsopg/index.js
+++ momsopg/index.js
@@ -1,7 +1,15 @@
 "use strict";
 
-function momsberegner(amount, moms = 1.25) {
-  console.log("beløb inkl moms", amount * moms);
-}
+// function momsberegner(amount, moms = 1.25) {
+//   console.log("beløb inkl moms", amount * moms);
+// }
 
-momsberegner(200);
+momsberegner(100, 25);
+
+function momsberegner(amount, moms) {
+  console.log(
+    `beløb med moms ${moms}%, der er beløbet med moms: ${
+      amount * (1 + moms / 100)
+    }`
+  );
+}
