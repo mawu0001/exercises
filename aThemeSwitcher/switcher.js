d2754e6f809629b40f12c3b185e9c9e1f6ade1fd
Marie Wulff <mawu0001@stud.kea.dk>
2024-10-07 15:16:35 +0200

    kkkkkkkk

--- aThemeSwitcher/switcher.js
+++ aThemeSwitcher/switcher.js
@@ -1,14 +1,19 @@
 const selector = document.querySelector("select");
 const bodyPointer = document.querySelector("body");
 
+// Aflæser den aktuelle værdi af det valgte element i dropdown-menuen. - kigger på "option"s værdi
 let themeValue = selector.options[selector.selectedIndex].value;
+
+// kør funktion, med valgte theme
 change(themeValue);
 
+// lytter på ændringer i drop-down menuen, og starter funktionen derefter
 selector.addEventListener("change", (event) => {
   themeValue = event.target.value;
   change(themeValue);
 });
 
+// bestemmer hvilket data-theme den giver afhænger af conditionals - if-sætningerne
 function change() {
   if (themeValue === "fall") {
     bodyPointer.dataset.theme = "fall";
