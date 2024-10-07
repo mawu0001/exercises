da32230b6c1676225cee310773f731bf68843d60
Marie Wulff <mawu0001@stud.kea.dk>
2024-10-07 12:56:27 +0200

    done john

--- profFilterMain/profFilter.js
+++ profFilterMain/profFilter.js
@@ -1,23 +1,24 @@
-import { $, $$ } from "../utils/dom.js";
 const curseWords = [
   { bad: "var", good: "const" },
   { bad: "float", good: "grid" },
   { bad: "marquee", good: "just don't" },
 ];
 
-let theText =
-  " Programmering er sjovt! Men husk altid at bruge var, float og marquee i moderne kodning.";
+let theSentIsSafe = false;
+let theText = document.querySelector("p").textContent;
+document.querySelector("button").addEventListener("click", replaceBadWords);
 
-const btn = $("button");
-
-logText();
-
-function logText() {
-  $("p").innerText = theText;
+function replaceBadWords() {
+  if (theSentIsSafe === false) {
+    curseWords.forEach((curseWord) => {
+      theText = theText.replaceAll(
+        curseWord.bad,
+        `<span>${curseWord.good}</span>`
+      );
+    });
+    document.querySelector("p").innerHTML = theText;
+    theSentIsSafe = true;
+  } else {
+    document.querySelector("dialog").showModal();
+  }
 }
-
-btn.addEventListener("mousedown", replaceWords());
-
-function replaceWords() {}
-
-// replaceWords()
